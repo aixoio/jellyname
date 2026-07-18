834b5cb82fc9baba3bf453df7cc945eb5b8eb057
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 23:24:17 +1000

    add clippy rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+#![deny(clippy::invalid_regex)]
+#![forbid(unsafe_code)]
+#![deny(clippy::redundant_clone)]
+#![deny(clippy::redundant_allocation)]
+#![cfg_attr(not(test), deny(clippy::unwrap_used))]
+#![cfg_attr(not(test), deny(clippy::expect_used))]
+
 pub mod cli;
 pub mod jellyname;
