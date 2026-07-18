7220d5768559669674f5cb27d370799ac629f620
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 17:42:11 +1000

    write rename command for movies

--- src/cli/commands/rename.rs
+++ src/cli/commands/rename.rs
@@ -1,6 +1,16 @@
 use std::{path::PathBuf, process::ExitCode};
 
 use clap::Subcommand;
+use owo_colors::OwoColorize;
+
+use crate::{
+    handle_error,
+    jellyname::{
+        config::{self, Config, ConfigData},
+        renamer,
+    },
+    match_error, return_error,
+};
 
 #[derive(Subcommand)]
 pub enum RenameSubcommand {
@@ -14,7 +24,28 @@ pub fn run(sub: RenameSubcommand) -> ExitCode {
 }
 
 fn rename_movie(path: PathBuf) -> ExitCode {
-    println!("{:?}", path.extension());
+    println!(
+        "{} {}",
+        "Loading config from",
+        config::CONFIG_FILENAME.italic().bold()
+    );
+    let config = match_error!(Config::read_config());
+    let ConfigData::Movie(data) = config.data() else {
+        return_error!("config is not for movies");
+    };
+    println!();
+
+    println!("{}", "Movie".bold().blue());
+    println!("  {}: {}", "Name".cyan(), data.name());
+    println!("  {}: {}", "Year".cyan(), data.year());
+
+    println!();
+
+    println!("Renaming...");
+
+    handle_error!(renamer::rename_movie(data, &path.to_string_lossy()));
+
+    println!("{}", "Done!".bright_green().bold());
 
     ExitCode::SUCCESS
 }
