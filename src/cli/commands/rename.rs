df19405c6818195e81e090ba804d64f18b7dc5ab
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 22:11:35 +1000

    add rename_series command

--- src/cli/commands/rename.rs
+++ src/cli/commands/rename.rs
@@ -7,6 +7,8 @@ use crate::{
     handle_error,
     jellyname::{
         config::{self, Config, ConfigData},
+        core::{convert_episode_to_config, extract_episodes},
+        descover::descover_series,
         renamer,
     },
     match_error, return_error,
@@ -15,11 +17,13 @@ use crate::{
 #[derive(Subcommand)]
 pub enum RenameSubcommand {
     Movie { path: PathBuf },
+    Series { path: PathBuf },
 }
 
 pub fn run(sub: RenameSubcommand) -> ExitCode {
     match sub {
         RenameSubcommand::Movie { path } => rename_movie(path),
+        RenameSubcommand::Series { path } => rename_series(path),
     }
 }
 
@@ -45,6 +49,39 @@ fn rename_movie(path: PathBuf) -> ExitCode {
     handle_error!(renamer::rename_movie(data, &path.to_string_lossy()));
 
     println!("{}", "Done!".bright_green().bold());
+
+    ExitCode::SUCCESS
+}
+
+fn rename_series(path: PathBuf) -> ExitCode {
+    println!(
+        "Loading config from {}",
+        config::CONFIG_FILENAME.italic().bold()
+    );
+    let config = match_error!(Config::read_config());
+    let ConfigData::Series(data) = config.data() else {
+        return_error!("config is not for series");
+    };
+    println!();
+
+    println!("{}", "Series".bold().blue());
+    println!("  {}: {}", "Name".cyan(), data.name());
+    println!("  {}: {}", "Year".cyan(), data.year());
+
+    println!();
+
+    println!("Walking...");
+
+    let Some(path) = path.to_str() else {
+        return_error!("missing path");
+    };
+    let paths = match_error!(descover_series(path, &config));
+
+    let episodes: Vec<_> = extract_episodes(&paths)
+        .map(convert_episode_to_config)
+        .collect();
+
+    println!("{episodes:#?}");
 
     ExitCode::SUCCESS
 }
