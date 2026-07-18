acd99fad3f590b648cfe2ded88bc92cd130ec58b
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 18:53:20 +1000

    add descover_series

--- src/jellyname/descover.rs
+++ src/jellyname/descover.rs
@@ -1,5 +1,30 @@
-use crate::jellyname::config::SeriesData;
+use std::path::PathBuf;
 
-pub fn descover_series(path: &str, data: &SeriesData) -> anyhow::Result<Vec<String>> {
-    Ok(vec![])
+use walkdir::WalkDir;
+
+use crate::jellyname::config::Config;
+
+pub fn descover_series(path: &str, config: &Config) -> anyhow::Result<Vec<PathBuf>> {
+    let mut paths: Vec<PathBuf> = vec![];
+
+    for entry in WalkDir::new(path) {
+        let entry = entry?;
+
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
+        let Some(ext) = entry.path().extension().and_then(|ext| ext.to_str()) else {
+            continue;
+        };
+        let ext = ext.to_lowercase();
+
+        if !config.targets().iter().any(|t| t.to_lowercase() == ext) {
+            continue;
+        }
+
+        paths.push(entry.into_path());
+    }
+
+    Ok(paths)
 }
