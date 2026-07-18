6a395687ae0486d9a605c7509744ee5c790b8289
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 13:43:39 +1000

    use clap in cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,13 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, name = "jellyname")]
 pub struct Cli {
+    #[command(subcommand)]
     pub command: Commands,
 }
 
+#[derive(Subcommand)]
 pub enum Commands {
     Init,
 }
