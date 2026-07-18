9e5d6acb3bbb19eaf162941c0ef568e6307a90c3
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-18 18:40:28 +1000

    add empty descover_series

--- src/jellyname.rs
+++ src/jellyname.rs
@@ -1,4 +1,5 @@
 pub mod config;
 pub mod core;
+pub mod descover;
 pub mod renamer;
 mod utils;
---
+++ src/jellyname/descover.rs
@@ -0,0 +1,5 @@
+use crate::jellyname::config::SeriesData;
+
+pub fn descover_series(path: &str, data: &SeriesData) -> anyhow::Result<Vec<String>> {
+    Ok(vec![])
+}
